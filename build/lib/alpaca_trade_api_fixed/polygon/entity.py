52d9426e11cf1151367f3bb5a611607d2b8ddea1
John Parkhill <jparkhill@artemiscm.com>
2020-02-14 14:13:10 -0600

    renamed

---
+++ build/lib/alpaca_trade_api_fixed/polygon/entity.py
@@ -0,0 +1,223 @@
+import pandas as pd
+import pprint
+
+NY = 'America/New_York'
+
+
+class Entity(object):
+    def __init__(self, raw):
+        self._raw = raw
+
+    def __getattr__(self, key):
+        if key in self._raw:
+            val = self._raw[key]
+            return val
+        return getattr(super(), key)
+
+    def __repr__(self):
+        return '{name}({raw})'.format(
+            name=self.__class__.__name__,
+            raw=pprint.pformat(self._raw, indent=4),
+        )
+
+
+class Agg(Entity):
+    def __getattr__(self, key):
+        if key in self._raw:
+            val = self._raw[key]
+            if key == 'day':
+                return pd.Timestamp(val, tz=NY)
+            elif key in ('timestamp', 'start', 'end'):
+                return pd.Timestamp(val, tz=NY, unit='ms')
+            return val
+        return getattr(super(), key)
+
+
+class Aggs(list):
+    def __init__(self, raw):
+        def rename_keys(tick, map):
+            return {
+                map[k]: v for k, v in tick.items()
+            }
+        lst_to_use = []
+        if raw['ticks'] is None:
+            print ('no ticks for aggregates')
+        else:
+            lst_to_use = [
+                Agg(rename_keys(tick, raw['map']))
+                for tick in raw['ticks']
+            ]
+        super().__init__(lst_to_use)
+        self._raw = raw
+
+    @property
+    def df(self):
+        if not hasattr(self, '_df'):
+            raw = self._raw
+            size = raw['aggType']
+            # polygon doesn't return in ascending order
+            # Do not rely on df.sort_values() as this library
+            # may be used with older pandas
+            keycol = 't' if size[0] == 'm' else 'd'
+            columns = ('o', 'h', 'l', 'c', 'v', keycol)
+            df = pd.DataFrame(
+                sorted(raw['ticks'], key=lambda d: d[keycol]),
+                columns=columns,
+            )
+            df.columns = [raw['map'][c] for c in df.columns]
+            if size[0] == 'm':
+                df.set_index('timestamp', inplace=True)
+                # astype is necessary to deal with empty result
+                df.index = pd.to_datetime(
+                    df.index.astype('int64') * 1000000,
+                    utc=True,
+                ).tz_convert(NY)
+            else:
+                df.set_index('day', inplace=True)
+                df.index = pd.to_datetime(
+                    df.index).tz_localize(NY)
+
+            df.sort_index(inplace=True)
+            self._df = df
+
+        return self._df
+
+
+class _TradeOrQuote(object):
+    '''Mixin for Trade and Quote'''
+
+    def __getattr__(self, key):
+        if key in self._raw:
+            val = self._raw[key]
+            if key == 'timestamp':
+                return pd.Timestamp(val, tz=NY, unit='ms')
+            return val
+        return getattr(super(), key)
+
+
+class _TradesOrQuotes(object):
+    '''Mixin for Trades and Quotes'''
+
+    def __init__(self, raw):
+        def rename_keys(tick, map):
+            return {
+                map[k]: v for k, v in tick.items()
+            }
+        lst_to_use = []
+        if raw['ticks'] is None:
+            print ('no ticks for _TradesOrQuotes')
+        else:
+            lst_to_use = [
+                unit_class(rename_keys(tick, raw['map']))
+                for tick in raw['ticks']
+            ]
+        unit_class = self.__class__._unit
+        super().__init__(lst_to_use)
+        self._raw = raw
+
+    @property
+    def df(self):
+        if not hasattr(self, '_df'):
+            raw = self._raw
+            columns = self.__class__._columns
+            df = pd.DataFrame(
+                sorted(raw['ticks'], key=lambda d: d['t']),
+                columns=columns,
+            )
+            df.columns = [raw['map'][c] for c in df.columns]
+            df.set_index('timestamp', inplace=True)
+            df.index = pd.to_datetime(
+                df.index.astype('int64') * 1000000,
+                utc=True,
+            ).tz_convert(NY)
+
+            df.sort_index(inplace=True)
+            self._df = df
+
+        return self._df
+
+
+class Trade(_TradeOrQuote, Entity):
+    pass
+
+
+class Trades(_TradesOrQuotes, list):
+    _columns = ('p', 's', 'e', 't', 'c1', 'c2', 'c3', 'c4')
+    _unit = Trade
+
+
+class Quote(_TradeOrQuote, Entity):
+    pass
+
+
+class Quotes(_TradesOrQuotes, list):
+    _columns = ('t', 'c', 'bE', 'aE', 'aP', 'bP', 'bS', 'aS')
+    _unit = Quote
+
+
+class Exchange(Entity):
+    pass
+
+
+class SymbolTypeMap(Entity):
+    pass
+
+
+class ConditionMap(Entity):
+    pass
+
+
+class Company(Entity):
+    pass
+
+
+class EntityList(list):
+    def __init__(self, raw):
+        super().__init__([
+            self._entity_class(o) for o in raw
+        ])
+        self._raw = raw
+
+
+class Dividend(Entity):
+    pass
+
+
+class Dividends(EntityList):
+    _entity_class = Dividend
+
+
+class Split(Entity):
+    pass
+
+
+class Splits(EntityList):
+    _entity_class = Split
+
+
+class Earning(Entity):
+    pass
+
+
+class Earnings(EntityList):
+    _entity_class = Earning
+
+
+class Financial(Entity):
+    pass
+
+
+class Financials(EntityList):
+    _entity_class = Financial
+
+
+class News(Entity):
+    pass
+
+
+class NewsList(EntityList):
+    _entity_class = News
+
+
+class Ticker(Entity):
+    pass
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('alpaca_trade_api/__init__.py', 'rb') as f:
+with open('alpaca_trade_api_fixed/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
@@ -14,16 +14,16 @@ with open('README.md') as readme_file:
     README = readme_file.read()
 
 setup(
-    name='alpaca-trade-api',
+    name='alpaca-trade-api-fixed',
     version=version,
     description='Alpaca API python client',
     long_description=README,
     long_description_content_type='text/markdown',
     author='Alpaca',
-    author_email='oss@alpaca.markets',
-    url='https://github.com/alpacahq/alpaca-trade-api-python',
+    author_email='a.wlezien@gmail.com',
+    url='https://github.com/zlex7/alpaca-trade-api-fixed',
     keywords='financial,timeseries,api,trade',
-    packages=['alpaca_trade_api', 'alpaca_trade_api.polygon'],
+    packages=['alpaca_trade_api_fixed', 'alpaca_trade_api_fixed.polygon'],
     install_requires=[
         'asyncio-nats-client',
         'pandas',
